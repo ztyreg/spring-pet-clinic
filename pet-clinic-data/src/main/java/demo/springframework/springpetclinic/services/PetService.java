f0b778d22549d2f2a40422cb35b828a15cad3a92
Ethan <ethzheng[at]gmail[dot]com>
2020-08-19 01:01:41 -0500

    add base entity

---
+++ pet-clinic-data/src/main/java/demo/springframework/springpetclinic/services/PetService.java
@@ -0,0 +1,14 @@
+package demo.springframework.springpetclinic.services;
+
+import demo.springframework.springpetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+}
---
+++ pet-clinic-data/src/main/java/demo/springframework/springpetclinic/services/VetService.java
@@ -0,0 +1,14 @@
+package demo.springframework.springpetclinic.services;
+
+import demo.springframework.springpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
