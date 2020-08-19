bb6e7ca3ec2d557fea97220bea2c97ff38d60831
Ethan <ethzheng[at]gmail[dot]com>
2020-08-19 15:48:18 -0500

    add owner controller

---
+++ pet-clinic-web/src/main/java/demo/springframework/springpetclinic/controllers/OwnerController.java
@@ -0,0 +1,15 @@
+package demo.springframework.springpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/owners")
+@Controller
+public class OwnerController {
+
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String listOwners() {
+
+        return "owners/index";
+    }
+}
